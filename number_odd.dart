218b8c888d95168a04fc65ef10e039b1f5658f1a
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-17 11:46:11 +0500

    four

--- number_odd.dart
+++ number_odd.dart
@@ -1,7 +1,29 @@
-//A four-digit integer is given. Find the number of even digits in it.
+//A four-digit integer is given. Find the number of odd digits in it.
 
 //Create a variable "var_int" and assign it a four-digit integer value.
 
-//Print the number of even digits in the variable "var_int".
+//Print the number of odd digits in the variable "var_int".
 
-void main() {}
+void main() {
+  int var_int = 1264;
+  int a = var_int%10;
+  int b = var_int~/10;
+  int c = b%10;
+  int d = b~/10;
+  int e = d%10;
+  int f = d~/10;
+  int k=0;
+  if (a%2!=0) {
+    k+=1;
+  }
+  if (c%2!=0) {
+    k+=1;
+  }
+  if (e%2!=0) {
+    k+=1;
+  }
+  if (f%2!=0) {
+    k+=1;
+  }
+  print(k);
+}
