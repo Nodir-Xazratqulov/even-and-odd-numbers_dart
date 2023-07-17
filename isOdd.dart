c8c473a8961554f25a0b855e70a1ccf50aff34b1
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-17 11:45:05 +0500

    two

--- isOdd.dart
+++ isOdd.dart
@@ -1,4 +1,11 @@
 // Create a variable "number" and assign it a integer value.
 
 // Print 1 if the number is odd, otherwise print 0.
-void main() {}
+void main() {
+  int raqam = 4;
+  if(raqam%2!=0){
+    print(1);
+  } else{
+    print(0);
+  }
+}
