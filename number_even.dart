b4d7526e1f97bc2493cf8ef6423a9d10d5f37ee4
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-17 11:45:38 +0500

    three

--- number_even.dart
+++ number_even.dart
@@ -4,4 +4,27 @@
 
 //Print the number of even digits in the variable "var_int".
 
-void main() {}
+
+void main() {
+  int var_int = 1264;
+  int a = var_int%10;
+  int b = var_int~/10;
+  int c = b%10;
+  int d = b~/10;
+  int e = d%10;
+  int f = d~/10;
+  int k=0;
+  if (a%2==0) {
+    k+=1;
+  }
+  if (c%2==0) {
+    k+=1;
+  }
+  if (e%2==0) {
+    k+=1;
+  }
+  if (f%2==0) {
+    k+=1;
+  }
+  print(k);
+}
